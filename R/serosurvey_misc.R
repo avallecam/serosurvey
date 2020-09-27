c177ec6148bf63dc5e340fbb0b63b5e9f43eb072
avallecam <avallecam@gmail.com>
2020-09-27 16:39:40 -0500

    add decimal to percent argument to unite function

--- R/serosurvey_misc.R
+++ R/serosurvey_misc.R
@@ -9,6 +9,7 @@
 #' @param digits_dot digits of point estimate to round
 #' @param digits_low digits of lower interval to round
 #' @param digits_upp digits of upper interval to round
+#' @param decimal_to_percent logical to multiply values by 100. TRUE by default.
 #'
 #' @export unite_dotwhiskers
 #' @export serosvy_extract_posterior
@@ -44,20 +45,27 @@ unite_dotwhiskers <- function(data,
                               variable_upp,
                               digits_dot=3,
                               digits_low=2,
-                              digits_upp=3) {
+                              digits_upp=3,
+                              decimal_to_percent=TRUE) {
 
   # combo to maintain variable name in a new variable
   c_var <- enquo(variable_dot)
   c_var_name_01 <- c_var %>% rlang::as_name() %>% str_c("unite1_",.)
   c_var_name_02 <- c_var %>% rlang::as_name() %>% str_c("unite2_",.)
 
-  data %>%
+  data_pre <- data %>%
     mutate(estim_tab={{variable_dot}},
            cilow_tab={{variable_low}},
-           ciupp_tab={{variable_upp}}) %>%
-    # from decimal to percentile
-    mutate_at(.vars = vars(estim_tab,cilow_tab,ciupp_tab),
-              .funs = funs(.*100)) %>%
+           ciupp_tab={{variable_upp}})
+
+  if (decimal_to_percent==TRUE) {
+    data_pre <- data_pre %>%
+      # from decimal to percentile
+      mutate_at(.vars = vars(estim_tab,cilow_tab,ciupp_tab),
+                .funs = funs(.*100))
+  }
+
+  data_out <- data_pre %>%
     # digits must be value specific
     mutate_at(.vars = vars(estim_tab),.funs = format,digits=digits_dot) %>%
     mutate_at(.vars = vars(cilow_tab),.funs = format,digits=digits_low) %>%
@@ -67,10 +75,12 @@ unite_dotwhiskers <- function(data,
               .funs = ~if_else(str_detect(.x,"NA"),NA_character_,.x)) %>%
     # two proposal
     mutate(
-      !!c_var_name_01 := str_c(estim_tab,"% (",cilow_tab," - ",ciupp_tab,")"),
-      !!c_var_name_02 := str_c(estim_tab,"%\n(",cilow_tab," - ",ciupp_tab,")")
+      !!c_var_name_01 := str_c(estim_tab," (",cilow_tab," - ",ciupp_tab,")"),
+      !!c_var_name_02 := str_c(estim_tab,"\n(",cilow_tab," - ",ciupp_tab,")")
     ) %>%
     select(-estim_tab,-cilow_tab,-ciupp_tab)
+
+  data_out
 }
 
 #' @describeIn unite_dotwhiskers priorización con dos covariables
